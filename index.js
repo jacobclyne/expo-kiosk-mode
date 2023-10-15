f0960b494ed0106d6bf67e29661a71497663c14c
J <j@clyne.design>
2023-10-15 22:15:01 +0400

    test

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-import { NativeModules } from 'react-native';
+import React, {Component} from 'react';
 
-const { KioskMode } = NativeModules;
+import {NativeModules, View, Keyboard, Platform, TouchableWithoutFeedback} from 'react-native';
 
-export default {
-    enableKioskMode: () => {
-        return KioskMode.enableKioskMode();
-    },
-    disableKioskMode: () => {
-        return KioskMode.disableKioskMode();
-    },
-};
+const KioskMode = NativeModules.KioskMode
+export default KioskMode;
+
+import _ from 'lodash';
+
+export class Immersive extends Component {
+    constructor(props, context) {
+        super(props, context)
+        this.autoKioskMode = _.debounce(KioskMode.autoKioskMode, 200);
+    }
+    componentWillMount() {
+        this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyboardDidHide);
+    }
+    _keyboardDidHide() {
+        KioskMode.autoKioskMode();
+    }
+
+    autoKioskMode() {
+        KioskMode.autoKioskMode();
+    }
+
+    render() {
+        return (this.autoKioskMode() || null);
+    }
+}
